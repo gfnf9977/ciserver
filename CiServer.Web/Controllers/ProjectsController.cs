07e1926a6c7d37bd7f5897cba244c28e45ca2961
gfnf9977 <145157618+gfnf9977@users.noreply.github.com>
2025-12-04 21:03:18 +0200

    adding simple realization

---
+++ CiServer.Web/Controllers/ProjectsController.cs
@@ -0,0 +1,43 @@
+using CiServer.Data;
+using CiServer.Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CiServer.Web.Controllers;
+
+public class ProjectsController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public ProjectsController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var projects = await _context.Projects.ToListAsync();
+        return View(projects);
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(Project project)
+    {
+        if (ModelState.IsValid)
+        {
+            project.ProjectId = Guid.NewGuid();
+            project.CreatedAt = DateTime.UtcNow;
+            
+            _context.Projects.Add(project);
+            await _context.SaveChangesAsync();
+            
+            return RedirectToAction(nameof(Index));
+        }
+        return View(project);
+    }
+}
